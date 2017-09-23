21224aa1297abfd75f20d2b15284ea7046a7f85f
Alexander Vega <vega.ken.developer@gmail.com>
2017-09-23 18:10:23 -0500

    before the installation of packages

---
+++ app.js
@@ -0,0 +1,155 @@
+var apiUser = 'your-api-user-id'; // you can get these at : https://habitica.com/#/options/settings/api
+var apiKey = 'your-api-token';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var requestify = require('requestify');
+
+var respuestaPrincipal; // donde se guardará la repsuesta al request de tareas
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('./public')); //para que el servidor tenga archivos estáticos para el front-end
+  // le permite al html acceder a los archivos en las carpetas js y css
+
+//INICIO
+app.get('/',function(req,res)
+{
+    res.sendFile(__dirname + '/index.html');
+});
+
+//CONSEGUIR TODAS LAS TAREAS
+app.get('/getTasks',function(req,res) 
+{
+  requestify.request('https://habitica.com/api/v3/tasks/user?type=todos', {
+    method : 'GET',
+    headers: {
+      'x-api-user' : apiUser,
+      'x-api-key' : apiKey
+    }
+  })
+  .then(function(response)
+  {
+    console.log("hay respuesta por habitica");
+    respuestaPrincipal = response.getBody(); // TO DO: falta verificar si fue una respuesta exitosa ( "response.code" agregar después como una función)
+    //console.log(respuestaPrincipal['data']);
+    res.json({data: respuestaPrincipal['data']}).end();
+  });
+});
+
+// AGREGANDO UNA TAREA
+app.post('/addTask', function (req,res) 
+{
+  var nameTask = req.body.text_task; //console.log(req.body.text_task);
+
+  requestify.request('https://habitica.com/api/v3/tasks/user', {
+    method : 'POST',
+    body: {
+  		text: nameTask,
+  		type: 'todo',
+      priority: '2'
+  	},
+    headers: {
+      'x-api-user' : apiUser,
+      'x-api-key' : apiKey
+    }
+  })
+  .then(function(response)
+  {
+    //console.log(response.code);
+    //console.log(response.getBody());
+    res.json({data: response.getBody()}).end();
+  });
+});
+
+// MARCAR UNA TAREA COMO HECHA
+app.post('/checkTask', function (req,res) 
+{
+  var taskId = req.body.id_task;
+  var checkTaskString = "https://habitica.com/api/v3/tasks/" + taskId + "/score/up";
+
+  requestify.request(checkTaskString, {
+    method : 'POST',
+    headers: {
+      'x-api-user' : apiUser,
+      'x-api-key' : apiKey
+    }
+  })
+  .then(function(response)
+  {
+    //console.log(response.code);
+    res.json({data: response.code}).end();
+  });
+});
+
+// BORRAR UNA TAREA
+app.post('/deleteTask', function (req,res) 
+{
+  var idTask = req.body.id_task;
+  var stringURL = "https://habitica.com/api/v3/tasks/" + idTask;
+
+  requestify.request(stringURL, {
+    method : 'DELETE',
+    headers: {
+      'x-api-user' : apiUser,
+      'x-api-key' : apiKey
+    }
+  })
+  .then(function(response)
+  {
+    res.json({data: response.code}).end();
+  });
+});
+
+// AGREGAR UNA SUBTAREA
+app.post('/addSubTask', function (req,res) 
+{
+  var idTask = req.body.id_task;
+  var nameSubTask = req.body.text_subTask;   //console.log(req.body.text_task);
+  var stringURL = "https://habitica.com/api/v3/tasks/" + idTask + "/checklist";
+
+  requestify.request(stringURL, {
+    method : 'POST',
+    body: {
+  		text: nameSubTask,
+  	},
+    headers: {
+      'x-api-user' : apiUser,
+      'x-api-key' : apiKey
+    }
+  })
+  .then(function(response)
+  {
+    res.json({code: response.code , data: response.getBody()}).end(); //envia codigo y respuesta (TO DO: usar codigo para el front-end)
+  });
+});
+
+// MARCAR O DESMARCAR UNA SUBTAREA 
+app.post('/checkSubTask', function (req,res) 
+{
+  var taskId = req.body.id_task;
+  var substaskId = req.body.id_subtask;
+
+  var checkSubTaskString = "https://habitica.com/api/v3/tasks/" + taskId + "/checklist/" + substaskId + "/score";
+  // ejm = https://habitica.com/api/v3/tasks/c563ad31-8964-42d8-8819-bebb23cf87c9/checklist/6d38b5b2-3f13-4007-a731-40fb9d1d4bae/score
+    // esos son ids de ejm de la tarea y subtarea correspondiente 
+  requestify.request(checkSubTaskString, {
+    method : 'POST',
+    headers: {
+      'x-api-user' : apiUser,
+      'x-api-key' : apiKey
+    }
+  })
+  .then(function(response)
+  {
+    //console.log(response.code);
+    res.json({data: response.code}).end();
+  });
+});
+
+
+app.listen(3000);
+console.log("servidor ejecutandose, entrar a http://localhost:3000");
---
+++ public/js/main.js
@@ -0,0 +1,260 @@
+$("document").ready(function(){
+  //CONSEGUIR Y MOSTRAR TODAS LAS TAREAS
+  $.ajax(
+	{
+		type: "GET",
+		url: window.location.href+"getTasks",
+		success: function(reply_data)
+		{
+      //console.log(reply_data.data);
+      reply_data.data.forEach(function(e,i) { // e : elemento como tal , i : indice del elemento
+
+        $("#contenedorPrincipal2").append(
+          "<div class='row filaTarea' id='filaTarea"+e.id+"'>"+
+            "<div class='col-xs-8 col-sm-8 col-md-8 col-lg-8'>"+
+              "<div id='nTarea"+e.id+"'>"+
+                "<p id='"+e.id+"' class='nombreTarea' ondblclick='checkTheTask(id)'>" + e.text + "</p>"+
+              "</div>"+
+            "</div>"+
+
+            "<span class='fa fa-plus col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons' id='addSubTask"+e.id+"' onclick='addSubTask(this.id)'></span>"+ // value='"+e.id+"' (never could get the attribute value)
+            "<span class='fa fa-pencil-square-o col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons'></span>"+ 
+            "<span class='fa fa-trash col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons' id='deleteTask"+e.id+"' onclick='deleteTask(this.id)'></span>"+ 
+            "<span class='fa fa-arrows-v col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons'></span>"+
+
+          "</div>");
+        //agregando notas de cada tarea, si las hay
+        if(e.notes)
+        {
+          $('#nTarea' + e.id).append("<p class='notaTarea'>"+e.notes+"</p>");
+        }
+        //agregando lista de subtareas como un form para cada tarea. TO DO : averiguar si es realmente necesario usar un form (nunca se hace un submit)
+        if(e.checklist.length)
+        {
+          $('#nTarea' + e.id).append(
+            "<form id='nForm"+e.id+"' class='formTareas'>"+
+            "</form>");
+
+          for(j=0;j<e.checklist.length;j++)
+          {
+            $('#nForm' + e.id).append( 
+              "<div class='checkbox'>"+
+                "<label class='nombreSubtarea'>"+
+                  "<input type='checkbox' value='"+e.id+"' name='"+e.checklist[j].id+"' id='nCheckBox"+e.id+e.checklist[j].id+"' class='checkChecked' onclick='checkboxCheckedFunction(value,name)'>"+ // id='nCheckBox"+i+j+"'
+                  "<span class='textSubtareaCheckbox'>"+e.checklist[j].text+"</span>"+
+                "</label>"+
+              "</div>");
+
+            if(e.checklist[j].completed == true) // si la tarea aún no esta hecha
+            {
+              $('#nCheckBox'+e.id+e.checklist[j].id).prop("checked", true);
+            }
+          }
+        }
+      });
+
+      $("#contenedorPrincipal2").sortable(
+        {
+          handle : ".fa-arrows-v" // elemento quien dirige la propiedad sortable
+        }
+      );
+    }
+  })
+
+  // AGREGAR TAREA AL INICIO CUANDO SE PRESIONA ENTER
+  $("#formAgregaTarea").on('submit',function(e){ 
+    e.preventDefault();
+    var newTask = $("#formTextNewTask"); // id del form : newTask.val()
+    var nameNewTask = newTask.val();
+    nameNewTask = nameNewTask.charAt(0).toUpperCase() + nameNewTask.slice(1);// si la primera letra fue en minúscula, convertirla a mayúscula
+    //TO DO : buscar por un caracter : ejm "|" y decidir que luego de eso se tomará el texto como nota para la tarea
+
+    $.ajax(
+    {
+      type: 'POST',
+      dataType: "json",
+      url: window.location.href+"addTask",  // toma el url actual y le agrega "addTask" : ejm de como queda :  http://localhost:3000/addTask
+      data: {text_task : nameNewTask},
+      success:
+        function( reply_data ) {
+
+          //console.log(reply_data.data);
+          //console.log("id : " + reply_data.data.data.id);
+
+          $("#contenedorPrincipal2").prepend(
+            "<div class='row filaTarea' id='filaTarea"+reply_data.data.data.id+"'>"+ // TO DO : mejorar la forma en como manejar esta respuesta (variables)
+                "<div class='col-xs-8 col-sm-8 col-md-8 col-lg-8'>"+
+                  "<div id='nTarea"+reply_data.data.data.id+"'>"+
+                    "<p id='"+reply_data.data.data.id+"' class='nombreTarea' ondblclick='checkTheTask(id)'>" + reply_data.data.data.text + "</p>"+
+                  "</div>"+
+                "</div>"+
+
+                "<span class='fa fa-plus col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons' id='addSubTask"+reply_data.data.data.id+"' onclick='addSubTask(this.id)'></span>"+ // value='"+e.id+"' (never could get the attribute value)
+                "<span class='fa fa-pencil-square-o col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons'></span>"+
+                "<span class='fa fa-trash col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons' id='deleteTask"+reply_data.data.data.id+"' onclick='deleteTask(this.id)'></span>"+ 
+                "<span class='fa fa-arrows-v col-xs-1 col-sm-1 col-md-1 col-lg-1 action-buttons'></span>"+
+            "</div>");
+
+          //console.log("Task agregado a la pagina correctamente");
+        }
+    })
+
+    $('#formTextNewTask').val(''); // limpiando el texto para una nueva tarea. se limpia fuera del ajax para agregarlas más rápidamente
+
+    //TO DO : llamar a una funcion que actualice un icono que indique que ya está en sincronia con el servidor de habitica, se pondria en la respuesta
+  })
+
+});
+
+//FUNCIONES QUE OCURRIRÁN LUEGO DE LA CARGA DE LA PÁGINA
+
+//MARCAR UNA TAREA COMO HECHA Y BORRARLA DE LA PAGINA
+function checkTheTask(id){
+  $("#filaTarea"+id).remove(); // da la impresión al usuario de que la tarea ya se borró (aunque aún la respuesta se dará en el background)
+
+  $.ajax(
+  {
+    type: 'POST',
+    dataType: "json",
+    url: window.location.href+"checkTask",
+    data: {id_task : id},
+    success:
+      function( reply_data ) {
+        if (reply_data.data == 200)
+        {
+            //console.log("task marcado con exito");
+            //TO DO: aqui se podria llamar a la funcion que actualice el icono que indica sincronia con el servidor de habitica
+        }
+      }
+  })
+}
+
+//ELIMINAR UNA TAREA Y BORRARLA DE LA PAGINA
+function deleteTask(id){
+  //esto no tendría que hacerse así si pudiera tomar el atributo "value" o "name" de un elemento, pero no he encontrado forma hasta ahora.
+  var checkString = id.indexOf("deleteTask");
+  if( checkString == 0 )
+  {
+    var idTaskTarget = id.slice(10);
+    $.ajax(
+    {
+      type: 'POST',
+      dataType: "json",
+      url: window.location.href+"deleteTask", //"http://ec2-54-211-182-70.compute-1.amazonaws.com:2017/ultimoValor",
+      data: {id_task : idTaskTarget},
+      success:
+        function( reply_data ) {
+          if (reply_data.data == 200)
+          {
+              console.log("Tarea eliminada con éxito");
+          }
+        }
+    })
+    $("#filaTarea"+idTaskTarget).remove();
+  }
+}
+
+//MARCAR O DESMARCAR UNA SUBTAREA
+function checkboxCheckedFunction(value, name) {
+    //console.log("tarea #" + value);
+    //console.log("subtarea #" + name);
+    $.ajax(
+    {
+      type: 'POST',
+      dataType: "json",
+      url: window.location.href+"checkSubTask",
+      data: {id_task : value, id_subtask : name},
+      success:
+        function( reply_data ) {
+          if (reply_data.data == 200)
+          {
+              //console.log("cambio subtask realizado");
+              //TO DO: aqui se podria llamar a la funcion que actualice el icono que indica sincronia con el servidor de habitica
+          }
+        }
+    })
+}
+
+// AGREGAR UNA SUBTAREA
+function addSubTask(id){
+  //esto no tendría que hacerse así si pudiera tomar el atributo "value" o "name", pero no he encontrado forma hasta ahora.
+  var checkString = id.indexOf("addSubTask");
+  if( checkString == 0 )
+  {
+    var idTaskTarget = id.slice(10);
+    //console.log(idTaskTarget);
+
+    $('#nTarea'+idTaskTarget).append(
+      "<form class='formnewsubtask'>"+
+        "<input type='text' placeholder='Add new subtask' id='newSubTask"+idTaskTarget+"' onkeypress='return addTheSubTask(event,id,value)' autocomplete='off'/>"+  //value='text here'
+      "</form>");
+  }
+}
+
+function addTheSubTask(e,id,value) { // id = idTaskParent || value = textSubTask
+    if (e.keyCode == 13) {
+        e.preventDefault(); // evitar que se mande un form
+        //console.log(id); //newSubTask-ID-ID-ID-ID
+        var checkString = id.indexOf("newSubTask");
+        if( checkString == 0 )
+        {
+          var idTaskTarget = id.slice(10);
+          //console.log(newString);
+
+          $.ajax(
+          {
+            type: 'POST',
+            dataType: "json",
+            url: window.location.href+"addSubTask",
+            data: {id_task : idTaskTarget , text_subTask : value}, 
+            success:
+              function( reply_data ) {
+                if (reply_data.code == 200)
+                {
+                    //console.log("subTask creado con exito");
+                    var subTasksTotal = reply_data.data.data.checklist;
+
+                    var textNewSubTask = subTasksTotal[subTasksTotal.length-1].text; // TO DO: averiguar si puedes reemplazar esto con value (que se recibe en esta funcion)
+                    var idNewSubTask = subTasksTotal[subTasksTotal.length-1].id;
+                    //console.log(subTasksTotal[subTasksTotal.length-1].text); //[-1].text
+
+                    //Si no existe el form que contiene otras subtareas anteriores, se crea uno. De lo contrario se agrega solamente la nueva subtarea.
+                    if ( $('#nForm'+idTaskTarget).length != 0 )
+                    {
+                      //console.log("existe form, solo agregar checkbox");
+                      $('#nForm' + idTaskTarget).append(
+                        "<div class='checkbox'>"+
+                          "<label class='nombreSubtarea'>"+
+                            "<input type='checkbox' value='"+idTaskTarget+"' name='"+idNewSubTask+"' id='nCheckBox"+idTaskTarget+idNewSubTask+"' class='checkChecked' onclick='checkboxCheckedFunction(value,name)'>"+ // id='nCheckBox"+i+j+"'
+                            "<span class='textSubtareaCheckbox'>"+textNewSubTask+"</span>"+
+                          "</label>"+
+                        "</div>");
+                    }
+                    else {
+                      //console.log("no existe form, se crea y se agrega checkbox");
+                      $('#nTarea' + idTaskTarget).append(
+                        "<form id='nForm"+idTaskTarget+"' class='formTareas'>"+
+                          "<div class='checkbox'>"+
+                            "<label class='nombreSubtarea'>"+
+                              "<input type='checkbox' value='"+idTaskTarget+"' name='"+idNewSubTask+"' id='nCheckBox"+idTaskTarget+idNewSubTask+"' class='checkChecked' onclick='checkboxCheckedFunction(value,name)'>"+ // id='nCheckBox"+i+j+"'
+                              "<span class='textSubtareaCheckbox'>"+textNewSubTask+"</span>"+
+                            "</label>"+
+                          "</div>"+
+                        "</form>");
+                    }
+                }
+              }
+          })
+          // si no quieres efecto de retraso por esperar respuesta de servidor... aquí deberías agregar la info
+            // de hecho debería estar antes del ajax para estar seguros que estos elementos existirán para cambiar sus valores y ids (modificarías con Jquery)
+          $('.formnewsubtask').remove(); // eliminar el form
+
+        }
+        /* 
+          esta parte nunca funciona, TO DO : averiguar por que
+          console.log("esto es lo que se escribio en la caja de agregar nueva subtarea: "); // lo que se escribio en el subtask
+          console.log(value);
+        */
+        return false;
+    }
+}
